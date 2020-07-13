4fdba7e6e708e53946feaa9d0453dfc0b3f2c9d8
Mark Serrano <mark@markserrano.me>
2020-07-13 01:04:12 -0500

    Add support for exporting as SRT

--- lib/captive/formats/srt.rb
+++ lib/captive/formats/srt.rb
@@ -3,5 +3,20 @@
 module Captive
   class SRT
     include Base
+
+    def to_s
+      string = String.new
+      @cue_list.each do |cue|
+        string << cue.number.to_s
+        string << "\n"
+        string << milliseconds_to_timecode(cue.start_time).gsub!('.', ',')
+        string << ' --> '
+        string << milliseconds_to_timecode(cue.end_time).gsub!('.', ',')
+        string << "\n"
+        string << cue.text
+        string << "\n\n"
+      end
+      string
+    end
   end
 end
